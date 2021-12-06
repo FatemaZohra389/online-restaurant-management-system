62ff48a622e5331f27f55396487c6f6850fb1554
FatemaZohra389 <rakhifatema983@gmail.com>
2021-12-07 00:56:47 +0600

    refactor

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { Card, Button, Row, Col , Form ,Container} from "react-bootstrap";
-import './LoginPage.css';
-
-
-
+import { Card, Button, Row, Col, Form, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./LoginPage.css";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   console.log(username, password);
@@ -16,51 +15,32 @@ const LoginPage = () => {
     setPassword(e.target.value);
   };
   const onClickLogin = () => {
-    alert('Login');
-  }
+    alert("Login");
+    navigate("/home");
+  };
   console.log(username, password);
   return (
-   
-    <div className="container">
-            <div className="app-wrapper">
-<div>
+    <div className="container login-page">
+      <div className="app-wrapper">
+        <div>
+          <h2 className="title">Log In</h2>
+        </div>
+        <form onSubmit={onClickLogin} className="from-wrapper">
+          <div className="name">
+            <label className="label">User name</label>
+            <input className="input" type="text" name="username" />
+          </div>
+          <div className="password">
+            <label className="label">Password</label>
+            <input className="input" type="text" name="password" />
+          </div>
 
-<h2 className="title" >Log In</h2>
-
-</div>
-<form className="from-wrapper">
-<div className="name">
-    <label className="label">User name</label>
-    <input 
-    className="input" 
-    type="text" 
-    name="username"
-    
-    
-    />
+          <div>
+            <button className="submit">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
-    <div className="password">
-    <label className="label">Password</label>
-    <input 
-    className="input"
-    type="text" 
-    name="password" 
-    
-    />
-    
-    </div>
-    
-<div>
-    <button className="submit" >Login</button>
-</div>
-  
-
- </form>
-</div>
-            </div>
-       
-
-
   );
 };
 
