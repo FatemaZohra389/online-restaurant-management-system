4f4d86ee6847fa6ff84946e61faceda85b3844fd
FatemaZohra389 <rakhifatema983@gmail.com>
2021-12-07 21:14:56 +0600

    update menu page

--- client/src/pages/Section/Menu.js
+++ client/src/pages/Section/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, Button, CardGroup, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 import img1 from "./../../assets/images/img1.jpg";
 import img2 from "./../../assets/images/img2.jpg";
@@ -48,21 +49,30 @@ const dummyData = [
   },
 ];
 function Menu() {
+  const navigate = useNavigate();
+  const onClickCart = () => {
+    navigate("/cart");
+  };
   return (
     <div className="menu-page">
       <Row xs={1} md={12} className="g-4">
-        {Array.from({ length: 4 }).map((_, idx) => (
+        {dummyData.map((menu, idx) => (
           <Col md={4}>
             <Card>
-              <Card.Img variant="top" src="img1" />
+              <Card.Img variant="top" src={menu.img} />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Title>{menu.name}</Card.Title>
                 <Card.Text>
                   This is a longer card with supporting text below as a natural
                   lead-in to additional content. This content is a little bit
                   longer.
                 </Card.Text>
               </Card.Body>
+              <Card.Footer>
+                <Button onClick={onClickCart} variant="warning">
+                  + Cart
+                </Button>
+              </Card.Footer>
             </Card>
           </Col>
         ))}
