398d25e1b33d05af37a0c74e43306929ea47c292
FatemaZohra389 <rakhifatema983@gmail.com>
2021-12-23 01:25:40 +0600

    menus crud operation

--- api/controllers/menu.controller.js
+++ api/controllers/menu.controller.js
@@ -14,3 +14,102 @@ exports.findAll = async (req, res) => {
     });
   }
 };
+
+exports.create = async (req, res) => {
+  try {
+    const data = await Menu.findAll();
+    res.send(data);
+  } catch (e) {
+    res.status(500).send({
+      error: e,
+      message: e.message || "Unexpected error",
+    });
+  }
+};
+
+exports.findById = async (request, res) => {
+  const params = request.params;
+  try {
+    const data = await Menu.findOne({
+      where: {
+        id: params.id,
+      },
+    });
+    if (data) {
+      res.send(data);
+    } else {
+      res.status(404).send({
+        message: "Menu not found!",
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({
+      error: e,
+      message: e.message || "Unexpected error",
+    });
+  }
+};
+
+exports.createMenu = async (request, res) => {
+  const params = request.body;
+  try {
+    const data = await Menu.create(params);
+    res.send(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({
+      error: e,
+      message: e.message || "Unexpected error",
+    });
+  }
+};
+
+exports.updateMenu = async (request, res) => {
+  const params = request.body;
+  try {
+    const data = await Menu.update(params, { where: { id: params.id } }); // database update query where id = 1/2/3/4
+    if (data[0] > 0) {
+      // In data, it shows the number of rows update in an array; [ 0 ] if no rows updated
+      const menuData = await Menu.findOne({
+        // run database get query where id = 1/2/3/4
+        where: {
+          id: params.id,
+        },
+      });
+      res.send(menuData); // send data to frontend
+    } else {
+      res.send({
+        message: "No menu updated",
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({
+      error: e,
+      message: e.message || "Unexpected error",
+    });
+  }
+};
+
+exports.removeMenu = async (req, res) => {
+  const params = req.params;
+  try {
+    const data = await Menu.destroy({ where: { id: params.id } });
+    if (data) {
+      res.send({
+        message: "Menu deleted",
+      });
+    } else {
+      res.send({
+        message: "Menu was not found / failed to delete",
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({
+      error: e,
+      message: e.message || "Unexpected error",
+    });
+  }
+};
--- api/routes/menus.js
+++ api/routes/menus.js
@@ -4,5 +4,10 @@ const menus = require("../controllers/menu.controller");
 
 // * Get all Menus
 router.get("/", menus.findAll);
+router.post("/", menus.createMenu);
+router.patch("/", menus.updateMenu);
+
+router.get("/:id", menus.findById);
+router.delete("/:id", menus.removeMenu);
 
 module.exports = router;
