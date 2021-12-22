5c1b6b6046ee54e42b36f76c47312fbfb1318480
FatemaZohra389 <rakhifatema983@gmail.com>
2021-12-22 23:59:24 +0600

    bug fix

--- client/src/shared/components/Modal/Modal.js
+++ client/src/shared/components/Modal/Modal.js
@@ -1,49 +1,34 @@
-import React,{useState} from 'react'
-import { Button} from 'react-bootstrap'
-            
-function Modal() {
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+const MenuModal = () => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-return (
-
-<div>
-  
-
   return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
+    <div>
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
 
-      <Modal show={show} onHide={handleClose} animation={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+        <Modal show={show} onHide={handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    </div>
   );
+};
 
-                </div>
-              )
-            }
-            
-            export default Modal
-            
- 
-    
-
-
-
-
-
-
+export default MenuModal;
