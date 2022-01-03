f77e44dc5a5dbb9b007912746118db3293d49105
FatemaZohra389 <rakhifatema983@gmail.com>
2022-01-04 02:49:58 +0600

    print order

--- client/src/pages/Section/Order.jsx
+++ client/src/pages/Section/Order.jsx
@@ -3,12 +3,35 @@ import { useDispatch, useSelector } from "react-redux";
 import { Table, Container, Button } from "react-bootstrap";
 import "./Order.scss";
 import { AiFillEye } from "react-icons/ai";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 import {
   fetchOrders,
   fetchUserOrders,
 } from "../../redux/reducers/orderReducer";
 import OrderView from "../../shared/components/Order/OrderView";
 import OrderReviewModal from "../../shared/components/Order/OrderReviewModal";
+
+const _exportPdf = () => {
+  html2canvas(document.querySelector(".capture")).then((canvas) => {
+    // document.body.appendChild(canvas); // if you want see your screenshot in body.
+    const imgData = canvas.toDataURL("image/png");
+    const pdf = new jsPDF();
+    const pageWidth = pdf.internal.pageSize.getWidth();
+    const pageHeight = pdf.internal.pageSize.getHeight();
+    const widthRatio = pageWidth / canvas.width;
+    const heightRatio = pageHeight / canvas.height;
+    const ratio = widthRatio > heightRatio ? heightRatio : widthRatio;
+    const canvasWidth = canvas.width * ratio;
+    const canvasHeight = canvas.height * ratio;
+
+    const marginX = (pageWidth - canvasWidth) / 2;
+    const marginY = (pageHeight - canvasHeight) / 2;
+    pdf.addImage(imgData, "PNG", marginX, marginY, canvasWidth, canvasHeight);
+    pdf.save("order_report.pdf");
+  });
+};
+
 const Order = () => {
   const [view, setView] = useState(false);
   const [showReview, setShowReview] = useState(false);
@@ -48,7 +71,17 @@ const Order = () => {
         </div>
         <hr />
         <Container fluid>
-          <Table striped borderless hover responsive size="sm">
+          {user?.data?.type === "admin" && (
+            <div
+              style={{
+                marginTop: 5,
+                marginBottom: 5,
+              }}
+            >
+              <Button onClick={() => _exportPdf()}>Print</Button>
+            </div>
+          )}
+          <Table bordered hover responsive size="sm" className="capture">
             <thead>
               <tr>
                 <th>Serial</th>
